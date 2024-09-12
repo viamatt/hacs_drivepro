ca1a64b4e34f29fdc317b8610dee3ecf83abf4dc
Matthew Willcock <matthewwillcock@me.com>
2024-09-12 11:21:18 +0100

    Initial Structure

---
+++ custom_components/hacs_drivepro/const.py
@@ -0,0 +1,8 @@
+"""Constants for integration_blueprint."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "DrivePro integration"
+ATTRIBUTION = "Data provided by https://www.drivepro.io"
